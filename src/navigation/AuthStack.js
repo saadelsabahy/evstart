4d052abf48a7f202ae8553c2ed73fbc07358cd2e
saadelsabahy <36341653+saadelsabahy@users.noreply.github.com>
2020-02-21 16:29:42 +0200

    test transition

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -4,12 +4,32 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Auth from '../screens/Auth';
 const AuthStack = () => {
    const Stack = createStackNavigator();
+   const config = {
+      animation: 'spring',
+      config: {
+         stiffness: 1000,
+         damping: 500,
+         mass: 3,
+         overshootClamping: true,
+         restDisplacementThreshold: 0.01,
+         restSpeedThreshold: 0.01,
+      },
+   };
    return (
       <Stack.Navigator
          screenOptions={{
             headerShown: false,
          }}>
-         <Stack.Screen name={'Login'} component={Auth} />
+         <Stack.Screen
+            name={'Login'}
+            component={Auth}
+            options={{
+               transitionSpec: {
+                  open: config,
+                  close: config,
+               },
+            }}
+         />
       </Stack.Navigator>
    );
 };
