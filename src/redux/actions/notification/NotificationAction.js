d94cd3d63b47d8eee8f16db6501dc71d31012451
unknown <saadelsabahy11@gmail.com>
2020-07-22 00:27:54 +0200

    ..

--- src/redux/actions/notification/NotificationAction.js
+++ src/redux/actions/notification/NotificationAction.js
@@ -1,173 +1,112 @@
-import firebase, { notifications } from 'react-native-firebase';
+import firebase, {notifications} from 'react-native-firebase';
 import {
-   ASSIGN_FCM_TOKEN,
-   RECEIVE_NOTIFICATION,
-   GET_NOTIFICATION_FAILED,
-   GET_NOTIFICATON_SUCCESS,
-   GET_NOTIFICATION_LOADER,
+  ASSIGN_FCM_TOKEN,
+  RECEIVE_NOTIFICATION,
+  GET_NOTIFICATION_FAILED,
+  GET_NOTIFICATON_SUCCESS,
+  GET_NOTIFICATION_LOADER,
 } from './NotificationTypes';
 import AsyncStorage from '@react-native-community/async-storage';
 let onRecieveNotificationListener, onOpenNotificationListener;
 export const getNotification = navigation => async (dispatch, getState) => {
-   navigation.navigate('Home');
-   //when app opend
-   onRecieveNotificationListener = firebase
-      .notifications()
-      .onNotification(async notification => {
-         console.log('open', notification);
+  navigation.navigate('Home');
 
-         const {
-            _data: { ParentId, StudentName, TimeStamp, Type, TransactionID },
-            _notificationId,
-         } = notification;
-         onReceiveNotification(dispatch, {
-            ParentId,
-            StudentName,
-            TimeStamp,
-            Type,
-            notificationId: _notificationId,
-            transactionId: TransactionID,
-         });
-      });
-   // when app open in background
-   onOpenNotificationListener = firebase
-      .notifications()
-      .onNotificationOpened(async notificationOpen => {
-         console.log('background', notificationOpen);
-         const {
-            notification: {
-               _data: { ParentId, StudentName, TimeStamp, Type, TransactionID },
-               _notificationId,
-            },
-         } = notificationOpen;
-         onReceiveNotification(dispatch, {
-            ParentId,
-            StudentName,
-            TimeStamp,
-            Type,
-            notificationId: _notificationId,
-            transactionId: TransactionID,
-         });
-      });
-   //open when app is closed
-   firebase
-      .notifications()
-      .getInitialNotification()
-      .then(async openWhenAppClosedListener => {
-         if (openWhenAppClosedListener) {
-            const {
-               notification,
-               notification: {
-                  _data: {
-                     ParentId,
-                     StudentName,
-                     TimeStamp,
-                     Type,
-                     TransactionID,
-                  },
-                  _notificationId,
-               },
-            } = openWhenAppClosedListener;
-
-            const lastOpenFromClosedId = await AsyncStorage.getItem(
-               'lastNotification'
-            );
-
-            if (_notificationId !== lastOpenFromClosedId) {
-               await AsyncStorage.setItem('lastNotification', _notificationId);
-               console.log('close', notification);
-               onReceiveNotification(dispatch, {
-                  ParentId,
-                  StudentName,
-                  TimeStamp,
-                  Type,
-                  notificationId: _notificationId,
-                  transactionId: TransactionID,
-               });
-            } else {
-               console.log('returned');
-
-               return;
-            }
-         }
-      })
-      .catch(e => console.log('recieve close app notification error', e));
+  //when app opend
+  onRecieveNotificationListener = firebase
+    .notifications()
+    .onNotification(async notification => {
+      console.log('open', notification);
+    });
+  // when app open in background
+  onOpenNotificationListener = firebase
+    .notifications()
+    .onNotificationOpened(async notificationOpen => {
+      console.log('background', notificationOpen);
+    });
+  //open when app is closed
+  firebase
+    .notifications()
+    .getInitialNotification()
+    .then(async openWhenAppClosedListener => {
+      console.log('closed');
+    })
+    .catch(e => console.log('recieve close app notification error', e));
 };
 // delete notification listeners when un mount
 export const deleteNotificationOnUnmount = () => {
-   onRecieveNotificationListener();
-   onOpenNotificationListener();
+  onRecieveNotificationListener();
+  onOpenNotificationListener();
 };
 // get all notification when app opend
 export const getAllNotifications = () => async (dispatch, getState) => {
-   try {
-      dispatch({ type: GET_NOTIFICATION_LOADER, payload: true });
-      const userId = await AsyncStorage.getItem('userId');
-      let getNotificationResponse = await getAllDataFromFireStore(userId);
-      dispatch({
-         type: GET_NOTIFICATON_SUCCESS,
-         payload: getNotificationResponse,
-      });
-   } catch (error) {
-      console.log('get notification error', error);
-      dispatch({ type: GET_NOTIFICATION_FAILED });
-   }
+  try {
+    dispatch({type: GET_NOTIFICATION_LOADER, payload: true});
+    const userId = await AsyncStorage.getItem('userId');
+    let getNotificationResponse = await getAllDataFromFireStore(userId);
+    dispatch({
+      type: GET_NOTIFICATON_SUCCESS,
+      payload: getNotificationResponse,
+    });
+  } catch (error) {
+    console.log('get notification error', error);
+    dispatch({type: GET_NOTIFICATION_FAILED});
+  }
 };
 // handle receive notification
 const onReceiveNotification = async (
-   dispatch,
-   { ParentId, StudentName, TimeStamp, Type, notificationId, transactionId }
+  dispatch,
+  {ParentId, StudentName, TimeStamp, Type, notificationId, transactionId},
 ) => {
-   dispatch({ type: GET_NOTIFICATION_LOADER, payload: true });
-   await addNotificationToFireStore(
-      ParentId,
-      StudentName,
-      TimeStamp,
-      Type,
-      notificationId,
-      transactionId
-   );
+  dispatch({type: GET_NOTIFICATION_LOADER, payload: true});
+  await addNotificationToFireStore(
+    ParentId,
+    StudentName,
+    TimeStamp,
+    Type,
+    notificationId,
+    transactionId,
+  );
 
-   const fireStoreData = await getAllDataFromFireStore(ParentId);
-   dispatch({ type: RECEIVE_NOTIFICATION, payload: fireStoreData });
+  const fireStoreData = await getAllDataFromFireStore(ParentId);
+  dispatch({type: RECEIVE_NOTIFICATION, payload: fireStoreData});
 };
 // add to fire store
 const addNotificationToFireStore = async (
-   ParentId,
-   StudentName,
-   TimeStamp,
-   Type,
-   notificationId,
-   transactionId
+  ParentId,
+  StudentName,
+  TimeStamp,
+  Type,
+  notificationId,
+  transactionId,
 ) => {
-   try {
-      await firebase
-         .firestore()
-         .collection(`${ParentId}`)
-         .add({
-            ParentId,
-            StudentName,
-            TimeStamp,
-            Type,
-            notificationId,
-            transactionId,
-         });
-   } catch (error) {
-      console.log('add to fire store error', error);
-   }
+  try {
+    await firebase
+      .firestore()
+      .collection(`${ParentId}`)
+      .add({
+        ParentId,
+        StudentName,
+        TimeStamp,
+        Type,
+        notificationId,
+        transactionId,
+      });
+  } catch (error) {
+    console.log('add to fire store error', error);
+  }
 };
 // get firestore data
 const getAllDataFromFireStore = async ParentId => {
-   let data = [];
-   await firebase
-      .firestore()
-      .collection(`${ParentId}`)
-      .get()
-      .then(querySnapshot => {
-         querySnapshot.forEach(doc => {
-            data.push(doc._data);
-         });
-      })
-      .catch(e => console.log('get firedata error', e));
-   return data;
+  let data = [];
+  await firebase
+    .firestore()
+    .collection(`${ParentId}`)
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        data.push(doc._data);
+      });
+    })
+    .catch(e => console.log('get firedata error', e));
+  return data;
 };
